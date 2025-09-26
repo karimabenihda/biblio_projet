92d08f0192140cc33e31d508d92f65f8518b363c
fatimamachay-pixel <fatimamachay5@email.com>
2025-09-26 11:42:12 +0100

    ajouter la 1er fonction dans la fiche books

---
+++ books.py
@@ -0,0 +1,5 @@
+def trier_année_de_publication(a):
+    livre_t=sorted(a)
+    return
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from data import livres,trier_année_de_publication
+# Trier les livres par année de publication
+
+
+
+
