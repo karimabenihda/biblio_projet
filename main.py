d2520333da7ab2514a4bf347dd0e77b85628adda
karima <karimabenihda@gmail.com>
2025-09-26 13:02:01 +0100

    add q3

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from data import utilisateurs
-from users import filtrer,to_majuscule
+from data import utilisateurs,aime_livres
+from users import filtrer,to_majuscule,livres_aimes
 
-print(filtrer(utilisateurs))
-print(to_majuscule(utilisateurs))
+# print(filtrer(utilisateurs))
+# print(to_majuscule(utilisateurs))
+# print(livres_aimes(utilisateurs,aime_livres))
+livres_aimes(utilisateurs,aime_livres)
--- users.py
+++ users.py
@@ -5,6 +5,30 @@ def filtrer(l):
     majeurs=list(filter(lambda u : u[3]>=18,l))
     return majeurs
 
+#2. Formater les noms complets en majuscules (map, lambda).
+
 def to_majuscule(l):
     nom_majuscule=list(map(lambda n :  n[2].upper(),l))
     return nom_majuscule
+
+#3. Créer un dictionnaire associant chaque utilisateur à ses livres aimés.
+
+def livres_aimes(users,aimes):
+    for user ,aime in zip(users,aimes) :
+        
+        if user[0]==aime[0]:
+            print(user[1],aime[1])
+      
+        # u=user[0]
+        # a=aime[0]
+        # print(user )
+        # print(aime )
+        # print(u,a)
+
+
+# [
+#      ((1, 'Alice', 'Dupont', 25), (1, '1984')),
+#        ((2, 'Bob', 'Martin', 17), (1, 'Le Petit Prince')), 
+#        ((3, 'Clara', 'Durand', 32), (3, 'Harry Potter')),
+#          ((4, 'David', 'Petit',20), (4, '1984'))
+# ]
