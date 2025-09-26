045d7ce05ffd20b121cdfc3895871955bbdc0423
fatimamachay-pixel <fatimamachay5@email.com>
2025-09-26 12:46:06 +0100

    ajouter FATIMA HAPPY

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from data import livres
 from books import trier_annee_publication
 # Trier les livres par année de publication
-for livre in livres :
-    livres_tries = trier_annee_publication(livres)
-    print(livres_tries["titre"], "→", livres_tries["année"])
+livres_tries = trier_annee_publication(livres)
+ 
 
 
 
